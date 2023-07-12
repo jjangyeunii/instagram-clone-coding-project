5b97f04c2b8fdcc64a1df517cb8213a5ef168b7b
jjangyeunii <euneun0927@naver.com>
2023-07-12 18:18:15 +0900

    layout - navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -14,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={openSans.className}>{children}</body>
+    <html lang="en" className={openSans.className}>
+      <body className="w-full max-w-screen-xl overflow-auto mx-auto">
+        <header className="sticky top-0 bg-white z-10 border-b">
+          <Navbar />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
