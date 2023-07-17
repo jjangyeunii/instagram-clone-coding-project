ef8f3a2024a7a89e6295d40975e6c19cf0a09138
jjangyeunii <euneun0927@naver.com>
2023-07-17 18:11:24 +0900

    followingbar - styling

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -7,12 +7,12 @@ import Avatar from "./Avatar";
 
 export default function FollowingBar() {
   const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
-  const following = data?.following;
-
-  console.log(following);
+  // const following = data?.following;
+  // const following = undefined;
+  const following = data?.following && [...data?.following, ...data?.following];
 
   return (
-    <section>
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
       {loading ? (
         <PropagateLoader size={8} color="red" />
       ) : (
@@ -21,12 +21,17 @@ export default function FollowingBar() {
         )
       )}
       {following && following.length > 0 && (
-        <ul>
+        <ul className="w-full flex gap-2">
           {following.map(({ image, username }) => (
             <li key={username}>
-              <Link href={`/user/${username}`}>
+              <Link
+                className="flex flex-col items-center w-20"
+                href={`/user/${username}`}
+              >
                 <Avatar image={image} highlight />
-                <p>{username}</p>
+                <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                  {username}
+                </p>
               </Link>
             </li>
           ))}
