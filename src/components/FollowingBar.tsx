9d126fbdcca461fce37253b3fff1de576503346e
jjangyeunii <euneun0927@naver.com>
2023-07-17 18:37:48 +0900

    followingbar - carousel library

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -4,12 +4,11 @@ import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
 import Link from "next/link";
 import Avatar from "./Avatar";
+import ScrollableBar from "./ui/ScrollableBar";
 
 export default function FollowingBar() {
   const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
-  // const following = data?.following;
-  // const following = undefined;
-  const following = data?.following && [...data?.following, ...data?.following];
+  const following = data?.following;
 
   return (
     <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
@@ -21,21 +20,20 @@ export default function FollowingBar() {
         )
       )}
       {following && following.length > 0 && (
-        <ul className="w-full flex gap-2">
+        <ScrollableBar>
           {following.map(({ image, username }) => (
-            <li key={username}>
-              <Link
-                className="flex flex-col items-center w-20"
-                href={`/user/${username}`}
-              >
-                <Avatar image={image} highlight />
-                <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
-                  {username}
-                </p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
