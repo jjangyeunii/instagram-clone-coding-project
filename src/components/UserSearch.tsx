3cd7003f0f190ee79a4f6e1901f917c4444cae5c
jjangyeunii <euneun0927@naver.com>
2023-07-27 14:45:57 +0900

    search user - component

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,14 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
+import { GridLoader } from "react-spinners";
 import useSWR from "swr";
 
 export default function UserSearch() {
-  // /api/search/${keyword}
-  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나 네임과 비교
-  // 검색하는 keyword가 없다면 /api/search -> 전체 유저
   const [keyword, setKeyword] = useState("");
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
-  return <></>;
+  const {
+    data: users,
+    isLoading: loading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>무언가가 잘못 되었음 😜</p>}
+      {loading && <GridLoader />}
+      {!loading && !error && users?.length === 0 && (
+        <p>찾는 사용자가 없음 😭</p>
+      )}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
