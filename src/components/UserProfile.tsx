92047fbaae558a5fb716dad33e7d92da38f0994d
jjangyeunii <euneun0927@naver.com>
2023-07-28 13:36:50 +0900

    user profile - component

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,9 +1,34 @@
 import { ProfileUser } from "@/model/user";
+import Avatar from "./Avatar";
+import FollowButton from "./FollowButton";
 
 type Props = {
   user: ProfileUser;
 };
 
 export default function UserProfile({ user }: Props) {
-  return <p>{user.username}</p>;
+  const { image, username, name, following, followers, posts } = user;
+  const info = [
+    { title: "posts", data: posts },
+    { title: "following", data: following },
+    { title: "followers", data: followers },
+  ];
+  return (
+    <section>
+      <Avatar image={image} highlight />
+      <div>
+        <h1>{username}</h1>
+        <FollowButton user={user} />
+        <ul>
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span>{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p>{name}</p>
+      </div>
+    </section>
+  );
 }
