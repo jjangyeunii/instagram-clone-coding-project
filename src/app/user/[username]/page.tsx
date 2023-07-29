74c9514523d0eeeca1fd5e8d9d47a7c2cb948139
jjangyeunii <euneun0927@naver.com>
2023-07-29 21:46:50 +0900

    user post - service

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,3 +1,4 @@
+import UserPosts from "@/components/UserPosts";
 import UserProfile from "@/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
 import { notFound } from "next/navigation";
@@ -9,5 +10,10 @@ type Props = {
 export default async function UserPage({ params: { username } }: Props) {
   const user = await getUserForProfile(username);
   if (!user) notFound();
-  return <UserProfile user={user} />;
+  return (
+    <>
+      <UserProfile user={user} />
+      <UserPosts user={user} />
+    </>
+  );
 }
