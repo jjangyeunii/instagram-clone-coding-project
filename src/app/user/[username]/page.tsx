3417fc030a3fe39c348ca13d64965d7436d8e7b9
jjangyeunii <euneun0927@naver.com>
2023-07-29 22:34:23 +0900

    user post - styling

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -11,9 +11,9 @@ export default async function UserPage({ params: { username } }: Props) {
   const user = await getUserForProfile(username);
   if (!user) notFound();
   return (
-    <>
+    <section className="w-full">
       <UserProfile user={user} />
       <UserPosts user={user} />
-    </>
+    </section>
   );
 }
