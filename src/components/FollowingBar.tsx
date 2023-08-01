6dd801b334a742421b60c26824bbcb48bb7a97d1
jjangyeunii <euneun0927@naver.com>
2023-08-01 13:39:57 +0900

    bookmarks - ui

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { HomeUser } from "@/model/user";
 import { PropagateLoader } from "react-spinners";
-import useSWR from "swr";
 import Link from "next/link";
 import Avatar from "./Avatar";
 import ScrollableBar from "./ui/ScrollableBar";
+import useMe from "@/hooks/me";
 
 export default function FollowingBar() {
-  const { data, isLoading: loading, error } = useSWR<HomeUser>("/api/me");
-  const following = data?.following;
+  const { user, isLoading: loading, error } = useMe();
+  const following = user?.following;
 
   return (
     <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto relative z-0">
