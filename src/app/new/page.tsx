47f5b07f8a2f8a4d79d4f5de32a828cc105cb31d
jjangyeunii <euneun0927@naver.com>
2023-08-03 14:30:03 +0900

    new post - dragging

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,18 @@
-export default function NewPostPage() {
-  return <div>New Post Page</div>;
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import NewPost from "@/components/NewPost";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+  return <NewPost user={session.user} />;
 }
---
+++ src/components/NewPost.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { AuthUser } from "@/model/user";
+import PostUserAvatar from "./PostUserAvatar";
+import FilesIcon from "./ui/icons/FilesIcon";
+import Button from "./ui/Button";
+import { ChangeEvent, useState } from "react";
+
+type Props = {
+  user: AuthUser;
+};
+
+export default function NewPost({ user: { username, image } }: Props) {
+  const [dragging, setDragging] = useState(false);
+  const [file, setFile] = useState<File>();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const files = e.target?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+  const handleDrag = (e: React.DragEvent) => {
+    if (e.type === "dragenter") {
+      setDragging(true);
+    } else if (e.type === "dragleave") {
+      setDragging(false);
+    }
+  };
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    setDragging(false);
+    const files = e.dataTransfer?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+  return (
+    <section>
+      <PostUserAvatar username={username} image={image || ""} />
+      <form>
+        <input
+          className="hidden"
+          type="file"
+          name="input"
+          id="input-upload"
+          accept="image/*"
+          onChange={handleChange}
+        />
+        <label
+          htmlFor="input-upload"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          <FilesIcon />
+          <p>Drag and Drop your image here or click</p>
+        </label>
+        <textarea
+          name="text"
+          id="input-text"
+          required
+          rows={10}
+          placeholder={"Write a caption..."}
+        ></textarea>
+        <Button text="Publish" onClick={() => {}} />
+      </form>
+    </section>
+  );
+}
