ff255498a7d338c965f905f76d3ba2745d26a4bc
jjangyeunii <euneun0927@naver.com>
2023-08-03 14:43:19 +0900

    new post - ui

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -5,6 +5,7 @@ import PostUserAvatar from "./PostUserAvatar";
 import FilesIcon from "./ui/icons/FilesIcon";
 import Button from "./ui/Button";
 import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 
 type Props = {
   user: AuthUser;
@@ -18,7 +19,6 @@ export default function NewPost({ user: { username, image } }: Props) {
     const files = e.target?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
   const handleDrag = (e: React.DragEvent) => {
@@ -37,13 +37,12 @@ export default function NewPost({ user: { username, image } }: Props) {
     const files = e.dataTransfer?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
   return (
-    <section>
+    <section className="w-full max-w-xl flex flex-col items-center mt-6">
       <PostUserAvatar username={username} image={image || ""} />
-      <form>
+      <form className="w-full flex flex-col mt-2">
         <input
           className="hidden"
           type="file"
@@ -53,16 +52,38 @@ export default function NewPost({ user: { username, image } }: Props) {
           onChange={handleChange}
         />
         <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && "border-2 border-sky-500 border-dashed"
+          }`}
           htmlFor="input-upload"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
           onDrop={handleDrop}
         >
-          <FilesIcon />
-          <p>Drag and Drop your image here or click</p>
+          {dragging && (
+            <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none" />
+          )}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FilesIcon />
+              <p>Drag and Drop your image here or click</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
         </label>
         <textarea
+          className="outline-none text-lg border border-neutral-300"
           name="text"
           id="input-text"
           required
